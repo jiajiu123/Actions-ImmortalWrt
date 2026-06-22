932eda69e44f77a484bb51603a83c509fd3162c5
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2026-06-22 22:56:29 +0800

    Update feed sources and remove obsolete Makefiles
    Removed v2ray-geodata and mosdns Makefiles, and cloned new packages.

--- before-installing-feed.sh
+++ before-installing-feed.sh
@@ -6,3 +6,9 @@ sed -i '/video/d' feeds.conf.default
 
 # Add feed sources
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2;main' >> feeds.conf.default
+
+# Add mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
