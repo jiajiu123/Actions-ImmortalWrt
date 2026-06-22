79a08d24037499de874325f529723bd5e51e41e5
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2026-06-22 22:53:25 +0800

    Update feed sources in before-installing-feed.sh
    Removed old feed sources and added new Passwall2 feed.

--- before-installing-feed.sh
+++ before-installing-feed.sh
@@ -5,6 +5,4 @@ sed -i '/telephony/d' feeds.conf.default
 sed -i '/video/d' feeds.conf.default
 
 # Add feed sources
-echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.default
-echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon' >> feeds.conf.default
-echo 'src-git wrtbwmon_luci https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2;main' >> feeds.conf.default
