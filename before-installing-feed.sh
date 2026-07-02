801844a037906e78e0c527b61ce17beeeed45c09
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2026-07-02 18:16:44 +0800

    Update before-installing-feed.sh

--- before-installing-feed.sh
+++ before-installing-feed.sh
@@ -3,6 +3,3 @@
 # Remove feed sources
 sed -i '/telephony/d' feeds.conf.default
 sed -i '/video/d' feeds.conf.default
-
-# Add feed sources
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2;main' >> feeds.conf.default
