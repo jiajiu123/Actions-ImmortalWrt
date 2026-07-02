c213a3da521e50c1034ad2c13ca53d04b82edc7f
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2026-07-02 18:51:12 +0800

    Update after-installing-feed.sh

--- after-installing-feed.sh
+++ after-installing-feed.sh
@@ -6,6 +6,9 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 # Partition alignment
 sed -i 's/256/4096/g' target/linux/x86/image/Makefile
 
+# Replace ash with bash
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
 # Fix qbittorrent recursive dependency
 rm -rf package/feeds/luci/luci-app-qbittorrent
 rm -rf feeds/luci/applications/luci-app-qbittorrent
